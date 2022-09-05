673d3e50a165dca6eb368ce262543007ace34464
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2022-09-05 11:58:47 -0400

    linter now happy with all files

--- restify_mining/plotter.py
+++ restify_mining/plotter.py
@@ -1,45 +1,65 @@
-# See https://stackoverflow.com/a/10138308
+"""
+Utils module to create nice gaussian (skill distribution in entire population) and boxplots (
+skill distribution in partitions). Deeply inspired by: https://stackoverflow.com/a/10138308
+"""
 
+import math
+import skills
 import matplotlib.pyplot as plt
 import numpy as np
-import scipy.stats as stats
-import math
-from matplotlib.pyplot import figure
+from scipy import stats
 
 
 def plot_gaussian(mean, stddev, colour):
-    # reset figure, to have separate drawings
-    # plt.clf()
+    """Plots the gaussian curves, based on provided skill mena and standard-deviation values for a
+    given skill. Call this function repeatedly to create an overlay figure with all skill
+    curves."""
 
-    # define frame size
-    # figure((8, 6), 80)
+    # Set figure dimensions
     plt.rcParams["figure.figsize"] = (14, 4)
 
     # actually plot the gaussian distributions
     sigma = math.sqrt(stddev)
-    x = np.linspace(mean - 3 * sigma, mean + 3 * sigma, 100)
-    plt.plot(x, stats.norm.pdf(x, mean, sigma), colour)
+    distr = np.linspace(mean - 3 * sigma, mean + 3 * sigma, 100)
+    plt.plot(distr, stats.norm.pdf(distr, mean, sigma), colour)
+
+    # Add axis labels
     plt.xlabel("Skill Score")
     plt.ylabel("Probability Density")
+
+    # Store on disk
     plt.savefig("generated-plots/gaussians.png")
 
 
-def plot_box(values, palette, partitions, filename):
+def plot_box(all_skill_values_by_skill_by_group: list[int], palette: list[str],
+             amount_partitions: int, filename: str):
+    """Prints a boxplot, based on a single array with all participant skill values, ordered by
+    skill index and group index. Illustration: first there come all values of participant s fo
+    group 1, skill one. Then come all values of participants of group 2, skill 1, and so on.
+    """
+
     # reset figure, to have separate drawings
     plt.clf()
 
     # define frame size
-    # figure((8, 6), 80)
     plt.rcParams["figure.figsize"] = (14, 4)
 
     # plot the boxes
-    for index in range(len(values)):
-        plotter_colour = palette[int(index/partitions)]
-        plt.boxplot(values[index], positions=[index + 1], notch=False, patch_artist=True, showfliers=True,
+    for index in range(len(all_skill_values_by_skill_by_group)):
+        plotter_colour = palette[int(index / amount_partitions)]
+        plt.boxplot(all_skill_values_by_skill_by_group[index], positions=[index + 1], notch=False,
+                    patch_artist=True,
+                    showfliers=True,
                     boxprops=dict(facecolor=plotter_colour, color="#FFFFFF"),
                     capprops=dict(color=plotter_colour),
                     whiskerprops=dict(color=plotter_colour),
                     flierprops=dict(color=plotter_colour, markeredgecolor=plotter_colour),
-                    medianprops=dict(color='#000000'), showmeans=True, meanprops={"marker":"s","markerfacecolor":"white", "markeredgecolor":plotter_colour})
-    plt.xticks([1 * partitions, 2  * partitions, 3  * partitions, 4  * partitions, 5  * partitions, 6  * partitions, 7  * partitions, 8  * partitions], ['Java', 'Spring', 'MVN', 'T.CORE', 'UNIX', 'REST', 'Singl.', 'Refl.'])
+                    medianprops=dict(color='#000000'), showmeans=True,
+                    meanprops={"marker": "s", "markerfacecolor": "white",
+                               "markeredgecolor": plotter_colour})
+    plt.xticks(
+        [1 * amount_partitions, 2 * amount_partitions, 3 * amount_partitions, 4 * amount_partitions,
+         5 * amount_partitions,
+         6 * amount_partitions, 7 * amount_partitions, 8 * amount_partitions],
+        skills.skill_tags)
     plt.savefig(filename)
---
+++ restify_mining/skills.py
@@ -0,0 +1,3 @@
+"""Helper module to define the default order of skills as strings"""
+
+skill_tags = ['Java', 'Spring', 'MVN', 'T.CORE', 'UNIX', 'REST', 'Singl.', 'Refl.']
