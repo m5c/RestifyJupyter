4056744602bf5012940d73d50db519dc9a0d1c44
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-02-16 11:28:53 -0500

    enabled all pseudo cells

--- jupyter_snippets/run_all_pseudo_cells.py
+++ jupyter_snippets/run_all_pseudo_cells.py
@@ -12,6 +12,10 @@ from jupyter_snippets.pseudo_cell_03_merge_csvs import cell_03
 from jupyter_snippets.pseudo_cell_04_compute_cgroup_skill_diffs import cell_04
 from jupyter_snippets.pseudo_cell_05_all_results_all_participants import cell_05
 from jupyter_snippets.pseudo_cell_05b_all_results_all_groups_radar import cell_05b
+from jupyter_snippets.pseudo_cell_06_all_tests_all_groups import cell_06
+from jupyter_snippets.pseudo_cell_07_time_quality_scatter import cell_07
+from jupyter_snippets.pseudo_cell_08_pre_time_quality_scatter import cell_08
+from jupyter_snippets.pseudo_cell_09_skill_quality_scatter import cell_09
 from restify_mining.utils.cache_clearer import clear_cache
 
 print("Clearing cached output files...")
@@ -44,21 +48,21 @@ print("Imitating Jupyter Pseudo Cell 05b...")
 cell_05b()
 print("Cell 05b complete.\n")
 
-# print("Imitating Jupyter Pseudo Cell 06...")
-# cell_06()
-# print("Cell 06 complete.\n")
-#
-# print("Imitating Jupyter Pseudo Cell 07...")
-# cell_07()
-# print("Cell 07 complete.\n")
-#
-# print("Imitating Jupyter Pseudo Cell 08...")
-# cell_08()
-# print("Cell 08 complete.\n")
-#
-# print("Imitating Jupyter Pseudo Cell 09...")
-# cell_09()
-# print("Cell 09 complete.\n")
+print("Imitating Jupyter Pseudo Cell 06...")
+cell_06()
+print("Cell 06 complete.\n")
+
+print("Imitating Jupyter Pseudo Cell 07...")
+cell_07()
+print("Cell 07 complete.\n")
+
+print("Imitating Jupyter Pseudo Cell 08...")
+cell_08()
+print("Cell 08 complete.\n")
+
+print("Imitating Jupyter Pseudo Cell 09...")
+cell_09()
+print("Cell 09 complete.\n")
 
 print("Success!")
 print("All Jupyter Cells imitated.")
