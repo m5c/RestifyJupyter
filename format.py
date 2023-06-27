4947a0d8bfddf9e955ba61aa40e82ab96367a792
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-06-27 14:22:03 +0200

    Added formatter script to fix auto corrupted notebooks

--- format.py
+++ format.py
@@ -1,10 +1,16 @@
 """
-This script fixes the auto-corrupted jupyter notebook.
-It searches for the multi line regex (illegal outputs statements for markdown cells) and wipes them.
+This script fixes the auto-corrupted jupyter notebooks, specifically removes empty outputs
+properties (which are not legal for markdown cells).
 """
 
 import re
+
 with open('Restify.ipynb', 'r') as file:
+    # Read original notebook file to string
     jupyter = file.read()
+
+    # Run a regex based search and replace, wipe all empty outputs properties.
     outputs_tag_removed = re.sub(',\n\s+\"outputs\":\ \[\]', '', jupyter)
-    print(outputs_tag_removed, file=open('Resitfy.ipynb', 'w'))
+
+    # Overwrite original notebook file
+    print(outputs_tag_removed, file=open('Restify.ipynb', 'w'))
