f1cf090d91c2ced209a5eab37f970d457fddf28f
Maximilian Schiedermeier <maximilian.schiedermeier@mail.mcgill.ca>
2023-08-14 10:27:35 -0400

    minor refactoring to avoid code duplication between cells

--- jupyter_snippets/pseudo_cell_09_time_and_passrate_boxplot.py
+++ jupyter_snippets/pseudo_cell_09_time_and_passrate_boxplot.py
@@ -26,8 +26,26 @@ def cell_09() -> None:
     # Step 1: Get hold of raw participant data, which we need to do whatever analysis. Divide
     # into groups.
     # The effective sample set is reduced, so it does not contain outliers (scammers)
+    partitioned_population: list[list[AssessedParticipant]] = partition_population()
+
+    # 2a) Extract times for individual methodologies, then produce plots
+    box_plot(MethodologyTimeExtractor, partitioned_population, "Time [sec]",
+             "generated-plots/09a-task-time-boxplot")
+    # 2b) Extract correctness for individual methodologies, then produce plots
+    box_plot(MethodologyPassrateExtractor, partitioned_population, "Test Passrate [%]",
+             "generated-plots/09b-task-passrate-boxplot")
+
+
+def partition_population() -> list[list[AssessedParticipant]]:
+    """
+    Creates a list of list where every outer list represetns participants of a group. The outcoma
+    has 6 entries, the last two ones each represeting the fused participants of previous groups
+    with matching tasks (only order different). The inner list entries are the assessed
+    participant objects.
+    """
     all_population: list[
         AssessedParticipant] = file_load_utils.load_all_assessed_participants(True)
+
     partitioned_population: list[list[AssessedParticipant]] = [
         filter_population_by_group(all_population, "red"),
         filter_population_by_group(all_population, "green"),
@@ -41,12 +59,7 @@ def cell_09() -> None:
     partitioned_population.append(partitioned_population[0] + partitioned_population[3])
     partitioned_population.append(partitioned_population[1] + partitioned_population[2])
 
-    # 2a) Extract times for individual methodologies, then produce plots
-    box_plot(MethodologyTimeExtractor, partitioned_population, "Time [sec]",
-             "generated-plots/09a-task-time-boxplot")
-    # 2b) Extract correctness for individual methodologies, then produce plots
-    box_plot(MethodologyPassrateExtractor, partitioned_population, "Test Passrate [%]",
-             "generated-plots/09b-task-passrate-boxplot")
+    return partitioned_population
 
 
 def box_plot(extractor_class: Extractor.__class__,
@@ -64,9 +77,7 @@ def box_plot(extractor_class: Extractor.__class__,
     # Step 4: produce reference point (so that plots have same axis scaling)
     # this also prints numeric stats data to console.
     graphical_box_plot(methodology_times["tc"], methodology_times["ide"],
-             group_tint_markers.group_tints.values(), label_name, filename)
-
-
+                       group_tint_markers.group_tints.values(), label_name, filename)
 
 
 def extract_methodology_metric(extractor: Extractor,
--- jupyter_snippets/pseudo_cell_10_fused_group_statistic_tests.py
+++ jupyter_snippets/pseudo_cell_10_fused_group_statistic_tests.py
@@ -1,6 +1,16 @@
+from jupyter_snippets.pseudo_cell_09_time_and_passrate_boxplot import partition_population
+from restify_mining.data_objects.assessed_participant import AssessedParticipant
+
+
 def cell_10() -> None:
     """
     Jupyter cell 10. See markdown description.
     :return: None
     """
-    print("===== Cell 10 =====")
+    partitioned_population: list[list[AssessedParticipant]] = partition_population()
+
+    # Run wilcoxon rank sum between orange BS / turquoise bookstore: time distribution (& passrate?)
+    # TODO: implement...
+    # orange_xox_samples: list[int] = partitioned_population[4]
+
+    # Run wilcoxon rank sum between orange xox / turquoise xox: time distribution (& passrate ?)
